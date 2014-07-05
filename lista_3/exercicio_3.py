68eb3037a2f7c167347884325fc6a2a0b6ceba8a
Kaio Cesar <tecnico.kaio@gmail.com>
2014-07-05 17:23:11 -0300

    Correção do erro do sistema

---
+++ lista_3/exercicio_3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+    Exercicio 3 - Lista 3 do curso Python para Zumbis
+    Supondo que a população de um país A seja da ordem de 80000 habitantes com uma taxa
+    anual de crescimento de 3% e que a população de B seja 200000 habitantes com uma taxa de
+    crescimento de 1.5%. Faça um programa que calcule e escreva o número de anos
+    necessários para que a população do país A ultrapasse ou iguale a população do país B,
+    mantidas as taxas de crescimento
+
+"""
+
